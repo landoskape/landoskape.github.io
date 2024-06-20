6b675ff3e390893a5529fdc6fd75ba2449a9077b
landoskape <andrewtylerlandau@gmail.com>
2024-06-20 09:58:49 +0100

    smarter scroll handling for navbar

--- assets/js/hide-navbar.js
+++ assets/js/hide-navbar.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     var header = document.querySelector("header");
     var lastScrollY = window.scrollY;
-    
+    var scrollThresholdHide = 7; 
+    var scrollThresholdShow = 0; 
+
     window.addEventListener("scroll", function() {
-        if (window.scrollY > lastScrollY) {
+        if (window.scrollY > lastScrollY + scrollThresholdHide) {
+            // Scrolled down decisively
             header.classList.add("hidden");
-        } else {
+        } else if (window.scrollY < lastScrollY - scrollThresholdShow) {
+            // Scrolled up decisively
             header.classList.remove("hidden");
         }
         lastScrollY = window.scrollY;
