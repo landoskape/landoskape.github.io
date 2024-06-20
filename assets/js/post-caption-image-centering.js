f51026ed83b7a85a46ae1afb5901c0ce365bbc24
landoskape <andrewtylerlandau@gmail.com>
2024-06-20 09:48:14 +0100

    fixing image size handling

--- assets/js/post-caption-image-centering.js
+++ assets/js/post-caption-image-centering.js
@@ -2,8 +2,19 @@ document.querySelectorAll('.post-caption').forEach(function(caption) {
     caption.addEventListener('mouseenter', function() {
         const captionRect = this.getBoundingClientRect();
         const imageRect = this.querySelector('.post-caption-image').getBoundingClientRect();
-        const topOffset = captionRect.top + captionRect.height + 10; // Adjust the '10' value as needed
+        const windowHeight = window.innerHeight;
+        const topOffset = captionRect.top - imageRect.height - 10; // Adjust the '10' value as needed
+        const bottomOffset = windowHeight - (captionRect.bottom + imageRect.height + 10); // Adjust the '10' value as needed
 
-        this.querySelector('.post-caption-image').style.top = `${topOffset}px`;
+        const spaceAbove = topOffset >= 0 ? topOffset : 0;
+        const spaceBelow = bottomOffset >= 0 ? bottomOffset : 0;
+
+        if (spaceAbove >= spaceBelow) {
+            // If there is more space above, position the image above
+            this.querySelector('.post-caption-image').style.top = `${topOffset}px`;
+        } else {
+            // If there is more space below, position the image below
+            this.querySelector('.post-caption-image').style.top = `${captionRect.bottom + 10}px`;
+        }
     });
 });
